934e02f6cfdb12dd3fede42f0daf0b9751d0190a
Nil <nilgallego@pm.me>
2024-02-12 18:16:38 +0100

    Remove unused elements from DashboardPage class

--- pages/dashboard.ts
+++ pages/dashboard.ts
@@ -2,9 +2,6 @@ import { Locator, Page, expect } from '@playwright/test'
 import { NavBar } from '../components/navBar'
 
 export class DashboardPage {
-    readonly dashboardTitle: Locator
-    readonly dashboardSubtitle: Locator
-    readonly logoutButton: Locator
     readonly navBar: NavBar
 
     constructor(public readonly page: Page) {
